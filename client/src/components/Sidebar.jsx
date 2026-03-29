516f5cc4ef5c795142ece468430ddbff8611285e
Vivek Dandotiya <vivekdandotiya772@gmail.com>
2026-03-29 16:21:46 +0530

    invite button problem1

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,3 +1,6 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import io from "socket.io-client";
 
 function Sidebar({ users, unread, setUnread, onlineUsers, refreshUsers, socket }) {
