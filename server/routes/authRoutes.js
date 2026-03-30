8f1402ea6add33d7b576c22014b08c9c7a7ad8d2
Vivek Dandotiya <vivekdandotiya772@gmail.com>
2026-03-30 18:03:32 +0530

    added diagnostic route for profile troubleshooting

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -8,6 +8,8 @@ router.post("/send-otp", authController.sendOTP);
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
 router.post("/profile", authController.updateUserProfile);
+router.get("/profile-test", (req, res) => res.json({ status: "Profile router is working" }));
 
 module.exports = router;
+
 
